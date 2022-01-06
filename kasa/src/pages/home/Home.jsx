106b6403ed29dee74cd9bcc16a2748bc28402e7e
Miguel Paredes Julien <julien314@hotmail.fr>
2022-01-06 18:58:18 +0100

    Création page home + ajout header et footer

---
+++ kasa/src/pages/home/Home.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Banner from "../../components/banner/Banner";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Location from "../../components/locations/Locations";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.titre = (
+      <h2>
+        Chez vous, <br />
+        partout et ailleurs
+      </h2>
+    );
+
+    this.state = {
+      data: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("data/data.json")
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        this.setState({ data: jsonResponse, isLoading: false });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    console.log(this.state.data);
+
+    return (
+      <React.Fragment>
+        <div className="mainWrapper">
+          <Header />
+          <div className="home">
+            <Banner titre={this.titre} />
+          </div>
+          <Location data={this.state.data} />
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
